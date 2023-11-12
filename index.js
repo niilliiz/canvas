39d5af3c12a7da15f500723072943537f244d9e5
niilliiz <niloo.vatan.nv@@gmail.com>
2023-11-12 07:11:04 +0330

    styling canvas

--- index.js
+++ index.js
@@ -5,4 +5,23 @@ canvas.height = window.innerHeight;
 let w = window.innerWidth;
 let h = window.innerHeight;
 
-let c = canvas.getContext("2d");
+let ctx = canvas.getContext("2d");
+
+let offset = 0;
+function draw() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.setLineDash([25, 50, 2]);
+  ctx.lineDashOffset = offset;
+  ctx.strokeRect(10, 10, 100, 100);
+}
+
+function march() {
+  offset++;
+  if (offset > 30) {
+    offset = 0;
+  }
+  draw();
+  setTimeout(march, 500);
+}
+
+march();
