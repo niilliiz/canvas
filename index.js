be32c465f6a7e3de1200bf8cb659a452cedf1bb7
niilliiz <niloo.vatan.nv@@gmail.com>
2023-11-18 13:18:40 +0330

    ant effect

--- index.js
+++ index.js
@@ -6,22 +6,3 @@ let w = window.innerWidth;
 let h = window.innerHeight;
 
 let ctx = canvas.getContext("2d");
-
-let offset = 0;
-function draw() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  ctx.setLineDash([25, 50, 2]);
-  ctx.lineDashOffset = offset;
-  ctx.strokeRect(10, 10, 100, 100);
-}
-
-function march() {
-  offset++;
-  if (offset > 30) {
-    offset = 0;
-  }
-  draw();
-  setTimeout(march, 500);
-}
-
-march();
