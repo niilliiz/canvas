819762991ef695606f0a9ed565637c4cf89a85d5
niilliiz <n.vatanara@mydoki.com>
2023-11-19 12:04:38 +0330

    gradient

--- index.js
+++ index.js
@@ -6,3 +6,12 @@ let w = window.innerWidth;
 let h = window.innerHeight;
 
 let ctx = canvas.getContext("2d");
+
+
+//GRADIENT
+
+// createLinearGradient(x1, y1, x2, y2)
+// createRadialGradient(x1, y1, r1, x2, y2, r2)
+// createConicGradient(angle, x, y)
+
+const linear=ctx.createLinearGradient(150,250,550,850)
